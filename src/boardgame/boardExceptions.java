7cf07ebdd097137cfae8b257325b4639000aa4c2
unknown <fabioclericuzilima@hotmail.com>
2023-08-27 17:04:53 -0300

    Criando os primeiros tratamentos de exceções para o tablueiro

---
+++ src/boardgame/boardExceptions.java
@@ -0,0 +1,11 @@
+package boardgame;
+
+public class boardExceptions extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	
+	public boardExceptions(String msg) {
+		super(msg);
+	}
+	
+}
